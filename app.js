538cd86e90897e58bdc5173a32073dd428397713
ariameli <ariadne.melissargos@heig-vd.ch>
2024-02-06 13:29:23 +0100

    first commit starter

---
+++ app.js
@@ -0,0 +1,14 @@
+//initialise le jeu
+//controle le flux principal des intercations
+
+//récupère donnée + les mélanges
+
+//initialise l'objet Game avec pays mélangés
+
+//Gère les interactions utilisateur via un Event Listener pour la soumission du formulaire.
+
+//Met à jour et affiche les scores et les meilleurs scores.
+
+//- Utilise la classe `Game` de `Game.js`.
+//- Manipule directement le DOM pour afficher les scores et les meilleurs scores.
+//- Lance des appels API pour récupérer les données des pays.
